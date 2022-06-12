a167636fd92c088bbe31643bafba728610e7595b
Asif Bhuiyan Shawon <90594450+AsifShawon@users.noreply.github.com>
2022-06-12 23:03:24 +0600

    Delete company directory

--- company/CH1_P5_integerOrNot.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-import java.util.Scanner;
-public class CH1_P5_integerOrNot {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.hasNextInt());
-    }
-}
