6e158194fe9ecbada6fdaeb08a709828098f477b
Asif Bhuiyan Shawon <asifbhshawon71@gmail.com>
2022-08-01 14:28:56 +0600

    Add files via upload

---
+++ Library/AddBook.java
@@ -0,0 +1,52 @@
+//import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class AddBook implements ActionListener{
+	JFrame frame2; 
+	
+	JTextField text;
+	JPanel panel ;
+	JButton b;
+	public String book;
+	public String getBook() {
+		return book;
+	}
+	
+	AddBook(){
+		frame2 =new JFrame("Add a Book");
+		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame2.setSize(420,550);
+		frame2.setLayout(null);
+		
+		text  = new JTextField();
+		text.setBounds(50,25,300,50);
+		
+		b = new JButton("Add");
+		b.addActionListener(this);
+		
+		panel = new JPanel();
+		panel.setBounds(50,100,300,300);
+		panel.setLayout(new GridLayout(4,4,10,10));
+		//panel.setBackground(Color.YELLOW);
+		panel.add(b);
+		
+		frame2.add(panel);
+		frame2.add(text);
+		frame2.setVisible(true);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==b) {
+			book = text.getText();
+			System.out.print(book);
+			frame2.dispose(); // to close the window when "add" clicked
+
+		}
+	}
+}
---
+++ Library/Library.java
@@ -0,0 +1,41 @@
+public class Library {
+	public int count = 4;
+	public String[] avBook = new String[100];
+
+	public Library() {
+		avBook[0] = "akash";
+		avBook[1] = "Aj Amar Mon Valo Nei";
+		avBook[2] = "Maa";
+		avBook[3] = "Three AM";
+	}
+
+//	void AvailableBooks() {
+//		for (int i = 0; avBook[i] != null; i++) {
+//			System.out.println((i + 1) + " " + avBook[i]);
+//		}
+//	}
+
+	public void setAvBook(String s) {
+		avBook[count] = s;
+		count++;
+	}
+	public String getAvBook(int i) {
+		return avBook[i];
+	}
+
+	public String getBook(int i) {
+		String s = avBook[i - 1];
+		String t[] = new String[100];
+		int k = count = 0;
+		for (int j = 0; avBook[j] != null; j++) {
+			if ((i - 1) == j)
+				continue;
+			t[k] = avBook[j];
+			k++;
+			count++;
+		}
+		this.avBook = t;
+		// System.out.print(count);
+		return s;
+	}
+}
