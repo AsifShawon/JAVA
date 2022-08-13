040dff9a40bff45b081919ef5d85f1e67cbc2ef4
Asif Bhuiyan Shawon <asifbhshawon71@gmail.com>
2022-08-13 21:30:08 +0600

    Add files via upload

---
+++ lab_tasks/Lab12_prac/main.java
@@ -0,0 +1,10 @@
+package Lab12_prac;
+
+public class main {
+	public static void main(String [] args) {
+		Triangle t = new Triangle(3, 3, 4, 5);
+		
+		System.out.println(t.perimeter());
+		System.out.println(t.area());
+	}
+}
